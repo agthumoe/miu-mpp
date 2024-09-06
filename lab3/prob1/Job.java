3435a2cc5c9d7a66c8aac4f0f23dfa2dac7f2853
Zaw Ye Naing <apple.zawyenaing@gmail.com>
2024-09-05 23:34:05 -0500

    Add Job class for composition

---
+++ lab3/prob1/Job.java
@@ -0,0 +1,11 @@
+package lesson3.labs.prob1;
+
+public class Job {
+    private double salary;
+    public Job(double salary) {
+        this.salary = salary;
+    }
+    public double getSalary() {
+        return salary;
+    }
+}
