26733a9779c5163c6127be53017432860a6d1ad4
Aung Thu Moe <agthumoe@gmail.com>
2024-09-18 14:03:11 -0500

    done question 3

--- Lab8/MyClass.java
+++ Lab8/MyClass.java
@@ -1,27 +1,34 @@
+import java.util.function.Predicate;
+
 public class MyClass {
-    private int x ;
+    public static void main(String[] args) {
+        MyClass myclass = new MyClass(1, "A");
+        MyClass myclass1 = new MyClass(1, "B");
+        myclass.myMethod(myclass); //
+        myclass.myMethod(myclass1);
+    }
+
+    private int x;
     private String y;
+
     public MyClass(int x, String y) {
         this.x = x;
         this.y = y;
     }
+
     // testing method to check the equality
     public void myMethod(MyClass cl) {
-        System.out.println(this.equals(cl));
+        Predicate<MyClass> p = this::equals;
+        System.out.println(p.test(cl));
     }
 
     @Override
     public boolean equals(Object ob) {
-        if(ob == null) return false;
-        if(ob.getClass() != getClass()) return false;
-        MyClass mc = (MyClass)ob;
+        if (ob == null)
+            return false;
+        if (ob.getClass() != getClass())
+            return false;
+        MyClass mc = (MyClass) ob;
         return mc.x == x && mc.y.equals(y);
-    }
-
-    public static void main(String[] args) {
-        MyClass myclass = new MyClass(1, "A");
-        MyClass myclass1 = new MyClass(1,"B");
-        myclass.myMethod(myclass); //
-        myclass.myMethod(myclass1);
     }
 }
