bef79232600572844b8bb136c79f6207fae546f1
Zayn <apple.zawyenaing@gmail.com>
2024-09-18 14:00:40 -0500

    Lab 8, problem 4.

---
+++ Lab8/problem4.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class problem4 {
+
+    public static void main(String[] args) {
+        String[] names = {"Alexis", "Tim", "Kyleen", "Kristy"};
+
+        //  a. Use Arrays.sort() to sort the names by ignore case using Method reference.
+        Arrays.sort(names, String::compareToIgnoreCase);
+
+        // b. Convert the sorted array into a List.
+        List<String> nameList = Arrays.asList(names);
+
+        // c. Print the sorted list using method reference
+        nameList.forEach(System.out::println);
+    }
+}
